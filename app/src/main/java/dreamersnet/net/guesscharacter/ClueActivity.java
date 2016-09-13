7a5fc166df46d5d26d1581d6440d91248a97b058
waterlgndx <bparker@dreamersnet.net>
2016-09-12 22:45:25 -0400

    Initial commit

---
+++ app/src/main/java/dreamersnet/net/guesscharacter/ClueActivity.java
@@ -0,0 +1,13 @@
+package dreamersnet.net.guesscharacter;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class ClueActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_clue);
+    }
+}
